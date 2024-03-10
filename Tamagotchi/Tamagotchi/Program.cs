5a749de22547774cb7c2a251bec7f8aab73d3e59
Анна <anna.mav31@gmail.com>
2024-03-10 20:32:06 +0700

    Создание интерфейса

---
+++ Tamagotchi/Tamagotchi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание экземпляра класса TamagotchiCondition.
+            TamagotchiCondition tamagotchiCondition = new TamagotchiCondition();
+            // Создание экземпляра класса Tamagotchi.
+            Tamagotchi tamagotchi = new Tamagotchi();
+            tamagotchiCondition.PrintInformationPet(tamagotchi);
+        }
+    }
+}
